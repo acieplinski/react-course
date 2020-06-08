ee58ef490e4c75fd624c8273e2f94f1894b43120
Adrian Cieplinski <a.cieplinski@gmail.com>
2020-06-08 14:44:55 +0200

    Revert "Using the useState() Hook to manipulate the functional component state"
    This reverts commit c8dcbf6cc0eb22cd6531acb49c82674289e7d006.
    
    going back to class component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Greg', age: 40 },
       { name: 'Kate', age: 25 }
     ]
-  });
+  }
 
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // not allowed: this.state.persons[0].name = 'Maximilian';
-    setPersonsState({
+    this.setState({
       persons: [
         { name: 'Maximilian', age: 28 },
         { name: 'Greg', age: 40 },
@@ -22,16 +22,18 @@ const app = props => {
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a new react app</h1>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Doing some other stuff</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-  );
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'));
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a new react app</h1>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Doing some other stuff</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'));
+  }
 }
 
-export default app;
+export default App;
