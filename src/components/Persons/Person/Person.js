52e514ee1d1039dcecd1b82859287e08c6d8c9f5
Adrian Cieplinski <a.cieplinski@gmail.com>
2020-06-17 13:25:34 +0200

    using Refs to access last element of the rendered persons list

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,12 +5,28 @@ import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/WithClass';
 
 class Person extends Component {
+    constructor() {
+        // not needed in commented snippet
+        super();
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         return (
             <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name} />
             </Aux>
         );
     }
