fcffdcedef3584d0179d80db5f836e1e6fc48c47
Adrian Cieplinski <a.cieplinski@gmail.com>
2020-06-17 02:46:37 +0200

    render adjacent JSX elements using Aux component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import styles from './Person.module.css';
+// import styles from './Person.module.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
     render() {
         return (
-            <div className={styles.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Aux>
         );
     }
 }
